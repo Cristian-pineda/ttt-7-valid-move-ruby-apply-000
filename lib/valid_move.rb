c547b9e8048b73718a057af2e1e3b0329ed2cff8
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 22:50:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == true
     true
-  elsif position_taken?(board, index) == false 
+  elsif position_taken?(board, index) == false
     false
   end
 end
