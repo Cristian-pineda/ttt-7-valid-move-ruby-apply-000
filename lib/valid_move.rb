9e8ed4e3f729d800bcafe7c03ea1ac2403b74c1f
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 23:10:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) == false
+  if index.between(1, 9) == false 
+    false
+  elsif position_taken?(board, index) == false
     true
   elsif position_taken?(board, index) == true
-    false
-  elsif board[index].between(1, 9) == false
     false
   end
 end
