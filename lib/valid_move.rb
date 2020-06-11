018f2df7ae4b5640fa08b3c5cbd69d627b173180
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-11 00:44:31 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
     false
   elsif board[index] == nil
     false
-  elsif board[index] == "X" || board[index] == "O"
-    true
+  elsif  index < 0 || index > 8
+    false
   end
 end
